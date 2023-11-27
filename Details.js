464ad387e0e527de6e86ade59051a617d680c1cd
Prakash V S <141955456+Prakash-V-S@users.noreply.github.com>
2023-11-28 01:24:59 +0530

    Update Details.js

--- Details.js
+++ Details.js
@@ -1,3 +1,7 @@
+//TASK : 06
+//DATE : 27/11/2023
+//03) Write a “person” class to hold all the details.-----------------------------------------
+
 class Person {
     constructor(name, age, curr, ug, clg, cgpa, skills) {
         this.name = name;
@@ -19,4 +23,10 @@ Thank You ............!`
 let detail = new Person("Prakash V S", 22, "GUVI FSD Course", "MCA", "SRM IST RAMAPURAM", 9.35, "HTML, CSS, JS, CORE JAVA, MYSQL, FIGMA");
 console.log(detail.getPersonDetails());
 
+// output ----------------------------------------
+
+// Hello Every one, Myself Prakash V S, 22 years old and I'm currently porsuing GUVI FSD Course 
+// and I completed MCA in SRM IST RAMAPURAM with 9.35 CGPA,
+// My Technical Skills are HTML, CSS, JS, CORE JAVA, MYSQL, FIGMA 
+// Thank You ............!
 
